1ef164b6fd3ffcfc844155bcf6ec25b5e3033b2e
Astral <ngreentux@hotmail.com>
2024-06-19 17:14:10 +1200

    Update flake.nix

--- Thinkpad T14 AMD Gen1/flake.nix
+++ Thinkpad T14 AMD Gen1/flake.nix
@@ -1,7 +1,6 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
   outputs = { self, nixpkgs }: {
-    # replace 'joes-desktop' with your hostname here.
     nixosConfigurations.astralthinkpad = nixpkgs.lib.nixosSystem {
       modules = [ ./configuration.nix ];
     };
