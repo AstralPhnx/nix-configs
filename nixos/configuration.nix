1b46598437dc3f130f633f2131146e94a1bc358c
AstralPhnx <ngreentux@hotmail.com>
2024-06-20 16:04:41 +1200

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -202,6 +202,7 @@
   gh
   #swayfx-unwrapped
   font-awesome
+  nerdfonts
 
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
