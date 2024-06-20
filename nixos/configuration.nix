1625db3eadfb132a3c26432e682c099d0a37a866
AstralPhnx <ngreentux@hotmail.com>
2024-06-20 18:26:30 +1200

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -33,7 +33,7 @@
   hardware.opengl.driSupport32Bit = true;
   programs.gamescope.enable = true;
   xdg.portal.enable = true;
-  xdg.portal.extraPortals = lib.mkForce [
+  xdg.portal.extraPortals = [
     #pkgs.xdg-desktop-portal-gtk # For both
     pkgs.xdg-desktop-portal-hyprland # For Hyprland
     pkgs.xdg-desktop-portal-gnome # For GNOME
@@ -209,7 +209,6 @@
   #swayfx-unwrapped
   font-awesome
   nerdfonts
-  #xdg-desktop-portal-gtk
   qadwaitadecorations-qt6
   qadwaitadecorations
   adw-gtk3
