e7c5c949cabe16ae22be6fb35653a5e77f8e7496
AstralPhnx <ngreentux@hotmail.com>
2024-06-20 14:24:23 +1200

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,7 +22,7 @@
   #  package = pkgs.kdePackages.sddm;
   #};
   #LightDM time actually
-  services.xserver.displayManager.lightdm.enable = true;
+  #services.xserver.displayManager.lightdm.enable = true;
   #Git Stuff
   programs.git.enable = true;
   programs.lazygit.enable = true;
@@ -118,7 +118,7 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
