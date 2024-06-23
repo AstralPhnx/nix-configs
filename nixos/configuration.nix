38793d21f86fa9a9812d61614a911ec4882aaf78
Astral <ngreentux@hotmail.com>
2024-06-23 14:01:15 +1200

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -215,6 +215,7 @@
   adwaita-qt
   adwaita-qt6
   gnome.dconf-editor
+  gnomeExtensions.ddterm
   
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
